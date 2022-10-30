7381059247046f8c2d4209fd492b70e65be5d282
Alexie-Campbell <alexie.campbell@mymona.uwi.edu>
2022-10-30 16:17:18 -0500

    Javascript updated

--- info2180-project1-starter-master/info2180-project1-starter-master/scripts.js
+++ info2180-project1-starter-master/info2180-project1-starter-master/scripts.js
@@ -1,1 +1,22 @@
 /* Add your JavaScript to this file */
+
+"use strict";
+
+
+window.onload= function(){
+var formEl = document.getElementsByTagName("FORM")[0];
+
+
+formEl.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    var email = document.getElementById("email").value; 
+    if (email==""){ 
+      alert("Please enter an email address"); 
+    }
+    else{
+        formEl.innerHTML="You have subscribed successfully using the email address: "+email;
+         
+    }
+
+});
+}
